ac92081042cf45e5e460594894bbe0a0415a17dc
Geor Guzitaev <georguzitaev@gmail.com>
2026-05-05 15:47:47 +0300

    Add project files.

---
+++ CoursePlatform.Db/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace CoursePlatform.Db
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ CoursePlatform.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CoursePlatform.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
